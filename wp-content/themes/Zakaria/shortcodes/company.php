2be1e63834f552987b0081b4c7573387cf1ae68b
Zakaria ELB <40664115+zakariaelb@users.noreply.github.com>
2021-11-17 16:51:06 +0000

    V1.01 17/11/21

---
+++ wp-content/themes/Zakaria/shortcodes/company.php
@@ -0,0 +1,58 @@
+<?php
+function company()
+{
+
+    $query = new WP_Query(
+        array(
+            'post_type' => 'company',
+            'post_status' => 'publish',
+            'post_per_page' => -1,
+            'order' => 'ASC',
+            'orderby' => 'menu_order'
+        )
+    );
+    $i = 1;
+    $str = '<div class="elementor-row company">';
+    while ($query->have_posts()):
+        $query->the_post();
+        $thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'company');
+        $str .= '
+         <div class="elementor-column elementor-col-33 elementor-top-column elementor-element " data-element_type="column">
+            <div class="elementor-column-wrap">
+                <div class="elementor-widget-wrap">
+                    <div class="image-wrapper">
+                                <a href="'.get_the_permalink().'" title="'. get_the_title().'">
+                                <img src="' . $thumbnail . '" alt="' . get_the_title() . '"></a>
+                            </div>
+                        <div class="company_info">          
+                            <h2>
+                                <a href="' . get_the_permalink() . '">' . get_the_title() . '</a>
+                            </h2>
+                            <h3>
+                            ' . do_shortcode('[acf field="title"]') . '
+                            </h3>
+                            <div>
+                            <a href="mailto:' . do_shortcode('[acf field="email_address"]') . '" title="Email ' . get_the_title() . '">
+                                ' . do_shortcode('[acf field="email_address"]') . '</a>
+            
+                            </div>
+                        <div>
+                            <a href="tel:' . do_shortcode('[acf field="phone_number"]') . '" title="Call ' . get_the_title() . '">
+                                ' . do_shortcode('[acf field="phone_number"]') . '</a>
+                        </div>
+                    </div> 
+                </div>
+            </div>             
+         </div>        
+         ';
+        if ($i % 3 == 0):
+            $str .= '</div>';
+            $str .= '<div class="elementor-row">';
+        endif;
+        $i++;
+    endwhile;
+    wp_reset_postdata();
+    return $str;
+}
+
+add_shortcode('company', 'company');
--- wp-content/themes/Zakaria/shortcodes/pro-areas.php
+++ wp-content/themes/Zakaria/shortcodes/pro-areas.php
@@ -1,5 +1,6 @@
 <?php
-function pro_skills_areas(){
+function pro_skills_areas()
+{
 
     $query = new WP_Query(
         array(
@@ -7,27 +8,27 @@ function pro_skills_areas(){
             'post_status' => 'publish',
             'post_per_page' => -1,
             'order' => 'ASC',
-            'orderby'=> 'menu_order'
+            'orderby' => 'menu_order'
         )
     );
     $i = 1;
     $str = '<div class="elementor-row">';
-        while ($query->have_posts()):
-         $query->the_post();
-         $str .= '
+    while ($query->have_posts()):
+        $query->the_post();
+        $str .= '
          <div class="elementor-column elementor-col-33 elementor-top-column elementor-element " data-element_type="column">
             <div class="pro-area-box homepage-skills">
                 <div class="content">
-                    <a class="icon" href="'.get_the_permalink().'">
-                    <i aria-hidden="true" class="'.do_shortcode('[acf field="Icon"]').'"></i></a>
-                        <h3 class="title">'.get_the_title().'</h3>
-                            <p class="description">'.do_shortcode('[acf field="blurb"]').'</p>
+                    <a class="icon" href="' . get_the_permalink() . '">
+                    <i aria-hidden="true" class="' . do_shortcode('[acf field="Icon"]') . '"></i></a>
+                        <h3 class="title">' . get_the_title() . '</h3>
+                            <p class="description">' . do_shortcode('[acf field="blurb"]') . '</p>
                                 </div>>
-            </div>>
-         </div>>        
+            </div>
+         </div>        
          ';
 
-        if($i % 3 == 0):
+        if ($i % 3 == 0):
             $str .= '</div>';
             $str .= '<div class="elementor-row">';
         endif;
@@ -44,51 +45,59 @@ function pro_skills_areas(){
 
 
 }
-add_shortcode('pro_skills_areas' ,'pro_skills_areas' );
 
-function pro_areas_link(){
+add_shortcode('pro_skills_areas', 'pro_skills_areas');
+
+function pro_areas_link()
+{
     $query = new WP_Query(
-            array(
-                'post_type' => 'pro-area',
-                'post_status' => 'publish',
-                'post_per_page' => -1,
-                'order' => 'ASC',
-                'orderby'=> 'menu_order'
-            )
+        array(
+            'post_type' => 'pro-area',
+            'post_status' => 'publish',
+            'post_per_page' => -1,
+            'order' => 'ASC',
+            'orderby' => 'menu_order'
+        )
     );
-        $links = '';
+    $links = '';
 
-            while ($query->have_posts()):
-                $query->the_post();
-                $links .= '<a href="'.get_the_permalink().'" title="'.get_the_title().'" >' .get_the_title().'</a><br>';
-            endwhile;
-        wp_reset_postdata();
-        return $links;
-    }
-add_shortcode('pro-areas_link' ,'pro_areas_link' );
+    while ($query->have_posts()):
+        $query->the_post();
+        $links .= '<a href="' . get_the_permalink() . '" title="' . get_the_title() . '" >' . get_the_title() . '</a><br>';
+    endwhile;
+    wp_reset_postdata();
+    return $links;
+}
+
+add_shortcode('pro-areas_link', 'pro_areas_link');
 add_filter('manage_pro-area_posts_columns', 'pro_area_columns');
-function pro_area_columns($columns) {
+function pro_area_columns($columns)
+{
 
     $columns = array(
-        'cb'=> 'cb',
-        'title'=>'title',
-        'order'=>'order',
-        'date'=>'date'
+        'cb' => 'cb',
+        'title' => 'title',
+        'order' => 'order',
+        'date' => 'date'
     );
     return $columns;
 }
+
 add_filter('manage_edit-pro-area_sortable_columns', 'pro_area_sortable_columns');
 
-function pro_area_sortable_columns ($columns){
+function pro_area_sortable_columns($columns)
+{
     $columns['order'] = 'order';
     return $columns;
 
-    }
+}
+
 add_action('manage_pro-area_posts_custom_column', 'pro_area_show_columns');
 
-function pro_area_show_columns($column_name){
+function pro_area_show_columns($column_name)
+{
     global $post;
-    if($column_name == 'order'):
+    if ($column_name == 'order'):
         echo $post->menu_order;
     endif;
-    }
+}
